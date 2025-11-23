9fa9353302f4f6e2c84c44354f1a53bb49e68445
Abhishek Raut <engineering.abhishek17@gmail.com>
2025-11-22 19:47:39 -0500

    Created module for loadbalancer
    Commit Type: feat
    Changes:
    - Provisioned Nodebalancer for cluster
    - Configured with tcp (80, 443) ports for HTTP/S traffic
    - Provisioned backend nodes with worker nodes only

---
+++ infrastructure/modules/loadbalancer/main.tf
@@ -0,0 +1,89 @@
+################################################################################
+# Project: BaseCamp
+# Loadbalancer Module
+#
+# Provisions core nodebalancer resources on Linode:
+#   - Nodebalancer to distribute traffic to cluster nodes
+#
+# Inputs:
+#   - project_name:             Project name
+#   - region:                   Linode cluster region
+#   - ssh_key:                  SSH public key to access instances
+#   - node_img:                 Linux Image label
+#   - talos_img:                Talos OS Image ID
+#   - cluster_node_type_id:     Compute instance type for cluster nodes
+#   - bastion_node_type_id:     Compute instance type for bastion host
+#   - dmz_subnet_id:            ID of the DMZ subnet
+#   - cluster_subnet_id:        ID of the Cluster subnet
+#   - worker_node_count:        Number of worker nodes in the cluster
+#
+# Outputs:
+#   - Bastion host node ID and public IP
+#   - Control plane and worker node IDs and private IPs
+################################################################################
+
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "~> 3.2.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+# ------------------------------------------------------------------------------
+# Provision nodebalancer in DMZ as gateway to cluster
+# ------------------------------------------------------------------------------
+resource "linode_nodebalancer" "gateway_lb" {
+  label                    = "${var.infra}-gateway-lb"
+  region                   = var.region
+  client_conn_throttle     = 20
+  client_udp_sess_throttle = 20
+}
+
+# ------------------------------------------------------------------------------
+# Configure nodebalancer ports
+# ------------------------------------------------------------------------------
+resource "linode_nodebalancer_config" "http" {
+  nodebalancer_id = linode_nodebalancer.gateway_lb.id
+  protocol        = "tcp"
+  port            = 80
+  algorithm       = "roundrobin"
+  stickiness      = "none"
+  check           = "connection"
+  check_interval  = 30
+  check_timeout   = 3
+  check_attempts  = 3
+}
+
+resource "linode_nodebalancer_config" "https" {
+  nodebalancer_id = linode_nodebalancer.gateway_lb.id
+  protocol        = "tcp"
+  port            = 443
+  algorithm       = "roundrobin"
+  stickiness      = "none"
+  check           = "connection"
+  check_interval  = 30
+  check_timeout   = 3
+  check_attempts  = 3
+}
+
+resource "linode_nodebalancer_config" "kubectlapi" {
+  nodebalancer_id = linode_nodebalancer.gateway_lb.id
+  protocol        = "tcp"
+  port            = 6443
+  algorithm       = "roundrobin"
+  stickiness      = "none"
+  check           = "none"
+}
+
+resource "linode_nodebalancer_config" "talosctlapi" {
+  nodebalancer_id = linode_nodebalancer.gateway_lb.id
+  protocol        = "tcp"
+  port            = 50000
+  algorithm       = "roundrobin"
+  stickiness      = "none"
+  check           = "none"
+}
+# ------------------------------------------------------------------------------
