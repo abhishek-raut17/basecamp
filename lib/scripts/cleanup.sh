59239bf64bafffce0059f86db0fc441c42964d08
Abhishek Raut <engineering.abhishek17@gmail.com>
2026-02-17 01:55:10 -0500

    Feature: copying systemd tuning files for gateways to DMZ host
    Commit Type: feat
    Changes:
    - adding config files for nginx, coturn and kernel tuning

--- lib/scripts/cleanup.sh
+++ lib/scripts/cleanup.sh
@@ -6,7 +6,6 @@
 #           - $TERRAFORM_VAR_DIR
 #           - $TALOS_SECRETS_DIR
 #           - $TALOS_DATA_DIR
-#           - $AGE_SECRETS_DIR
 #
 # ------------------------------------------------------------------------------
 
@@ -27,7 +26,6 @@ TERRAFORM_VAR_DIR="${TERRAFORM_VAR_DIR:-${DATA_DIR}/var/terraform}"
 TERRAFORM_STATE_DIR="${TERRAFORM_STATE_DIR:-${DATA_DIR}/state/terraform}"
 TALOS_DATA_DIR="${TALOS_DATA_DIR:-${DATA_DIR}/talos}"
 TALOS_SECRETS_DIR="${TALOS_SECRETS_DIR:-${SECRETS_DIR}/talos}"
-AGE_SECRETS_DIR="${AGE_SECRETS_DIR:-${SECRETS_DIR}/age}"
 
 cleanup() {
     log_mark "Cleaning up resources created during make run"
@@ -45,9 +43,6 @@ cleanup() {
 
     # Delete content from $TALOS_SECRETS_DIR
     rm -rf ${TALOS_SECRETS_DIR}/*
-
-    # Delete content from $AGE_SECRETS_DIR
-    rm -rf ${AGE_SECRETS_DIR}/*
 
     log_success "Cleaned up resources from last make run. Proceed for a fresh run."
 }
